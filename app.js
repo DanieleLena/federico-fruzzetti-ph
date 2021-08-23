392634e124cd22773ce5190014f16a30651ab2eb
DanieleLena <daniele.lena@yahoo.it>
2021-08-23 19:55:23 +0200

    Implemented mobile menu animation

--- app.js
+++ app.js
@@ -61,3 +61,43 @@ function openMenu(x, y) {
 
   menu.style.visibility = "visible";
 }
+
+
+//animation
+
+const burgerBtn = document.querySelector(".burger-icon");
+const tl = gsap.timeline({paused:true});
+
+function openNav() {
+  animateOpenNav();
+  
+  burgerBtn.onclick = function (e) {
+    // Toggle reversed to it's opposite value
+    tl.reversed(!tl.reversed());
+    // Use the toggle method in the classList API
+    document.body.classList.toggle("disable-scrolling");
+  };
+}
+function animateOpenNav() {
+  tl.to(".mobile-menu", {
+    onStart() {
+      console.log('ciao cazzoni');
+    },
+    duration: .3,
+    ease: "power3.out",
+  }).to(".mobile-menu", {
+    duration: .3,
+    ease: "power3.out",
+    opacity: .9
+  }).to(".mobile-link", {
+    opacity: 1,
+    duration: 1,
+    stagger: {
+      each: 0.1,
+      ease: "power1.in"
+    }
+  })
+  .reverse(); 
+}
+
+openNav();
