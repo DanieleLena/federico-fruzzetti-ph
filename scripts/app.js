0d09f614858db709ca21c41592bca26ca5c854bd
DanieleLena <daniele.lena@yahoo.it>
2021-08-25 18:34:10 +0200

    Add projects page

--- scripts/app.js
+++ scripts/app.js
@@ -107,23 +107,3 @@ function preloadImage(url, target) {
 
 
 
-//NAV BAR ANIMATION ================================================================
-
-const nav = document.querySelector(".nav");
-let lastScrollTop = 0;
-
-document.addEventListener("scroll", () => {
-  var st = window.pageYOffset || document.documentElement.scrollTop;
-
-  if (st > lastScrollTop) {
-    // downscroll code
-    nav.classList.add("hide-nav");
-    console.log("top");
-  } else {
-    // upscroll code
-        nav.classList.remove("hide-nav");
-
-    console.log("bottom");
-  }
-  lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-})
---
+++ scripts/projects.js
@@ -0,0 +1,2 @@
+
+gsap.to(".effect-lily", {opacity: 1,duration: 2 ,stagger: 0.2});
